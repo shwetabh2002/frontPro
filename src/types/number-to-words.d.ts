a8502ea038f39ea41d7cc7a0bac0bf9fafd4d78d
shwetabh2002 <shwetabhgupta2002@gmail.com>
2025-11-23 22:43:26 +0530

    Add Type Definitions for Number to Words Module
    - Created a new TypeScript declaration file for the 'number-to-words' module.
    - Defined functions for converting numbers to words, ordinal words, and ordinal words in English.

---
+++ src/types/number-to-words.d.ts
@@ -0,0 +1,6 @@
+declare module 'number-to-words' {
+  export function toWords(num: number): string;
+  export function toWordsOrdinal(num: number): string;
+  export function toWordsOrdinalEn(num: number): string;
+}
+
